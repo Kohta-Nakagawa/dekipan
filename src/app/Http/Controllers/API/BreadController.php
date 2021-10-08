b41d654b83e8ea985bad849eeca4b81b94bb29bc
hiraokakoichi <the10pointsflavor0327@gmail.com>
2021-10-08 21:36:07 +0900

    中川さんがテストで成功した形式でJsonを返すよう変更

--- src/app/Http/Controllers/API/BreadController.php
+++ src/app/Http/Controllers/API/BreadController.php
@@ -47,6 +47,84 @@ class BreadController extends Controller
     //     return $this->resConversionJson($result);
     // }
 
+    public function testData()
+    {
+        $result = 'testData: {
+            dekitateList: [
+              {
+                id: "8392830",
+                area: "sawaraku",
+                time: "3:31",
+                pan: "メロンパン",
+                shop: "ケイズオーブン2",
+                address: "福岡市中央区天神1-1-1",
+                image: "https://www.hokuo-tokyo.jp/upfiles/L1506066493_46-whole.jpg"
+              },
+              {
+                id: "8dg2830",
+                area: "nishiku",
+                time: "10:42",
+                pan: "ウインナーパン",
+                shop: "オーブンオーブン",
+                address: "福岡市中央区天神1-1-1",
+                image:
+                  "https://media-01.creema.net/user/245416/exhibits/3840354/1_7c71e56d0352b4aabf03780a5a7715a0_500.jpg"
+              },
+              {
+                id: "839gddde30",
+                area: "nishiku",
+                time: "3:56",
+                pan: "コッペパン",
+                shop: "ここに決めた",
+                address: "福岡市中央区天神1-1-1",
+                image:
+                  "https://coubic-images.s3.amazonaws.com/resources/265675/p1280x1280_61881c5d.JPG"
+              },
+              {
+                id: "839ge30",
+                area: "nishiku",
+                time: "1:56",
+                pan: "チョココロネ",
+                shop: "ふじや",
+                address: "福岡市中央区天神1-1-1",
+                image:
+                  "https://takatsuki-scramble.com/wp-content/uploads/2015/06/IMG_3868.jpg"
+              },
+              {
+                id: "83gegew29ge30",
+                area: "sawaraku",
+                time: "4:59",
+                pan: "食パン",
+                shop: "美味しいパン屋",
+                address: "福岡市中央区天神1-1-1",
+                image:
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQPV8Gg52EG6k9khkXaYRWwnHHuvoAvEdkp5g&usqp=CAU"
+              },
+              {
+                id: "83argh9ge30",
+                area: "nishiku",
+                time: "5:34",
+                pan: "あんぱん",
+                shop: "パン屋A",
+                address: "福岡市中央区天神1-1-1",
+                image:
+                  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcStvhVhukN4zD10o25PiCZQ_BF0CRQVZkW2Rg&usqp=CAU"
+              },
+              {
+                id: "839efege30",
+                area: "sawaraku",
+                time: "9:45",
+                pan: "揚げパン",
+                shop: "揚げパンや",
+                address: "福岡市中央区天神1-1-1",
+                image:
+                  "https://petit-mall.jp/hbrecipe/wp-content/uploads/yakiin2.jpg"
+              }
+            ]
+          }';
+        return $result;
+    }
+
     private function resConversionJson($result, $statusCode=200)
     {
         if(empty($statusCode) || $statusCode < 100 || $statusCode >= 600){
